e0bb32dacee7fa23d93e31ef6cb193ac8c0e6893
overuou <120343055+overuou@users.noreply.github.com>
2025-10-01 02:41:42 +0300

    removed ton

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,16 +1,16 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Section, Cell, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
 import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
-import tonSvg from './ton.svg';
+// import tonSvg from './ton.svg';
 
 export const IndexPage: FC = () => {
   return (
     <Page back={false}>
       <List>
-        <Section
+        {/* <Section
           header="Features"
           footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
         >
@@ -22,7 +22,7 @@ export const IndexPage: FC = () => {
               TON Connect
             </Cell>
           </Link>
-        </Section>
+        </Section> */}
         <Section
           header="Application Launch Data"
           footer="These pages help developer to learn more about current launch information"
